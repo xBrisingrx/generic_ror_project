d40b60a7b639d5bb3422aa01e2f83abb6d15497c
Mauro Almiron <mdavid.almiron@gmail.com>
2023-10-15 20:17:52 -0300

    removed debugger, I used them to verify behavior when submied is success

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -15,7 +15,6 @@ export default class extends Controller {
     document.body.classList.remove("overflow-hidden")
   }
   submitEnd(e) {
-    debugger
     if (e.detail.success) {
       this.hideModal()
     }

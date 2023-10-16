cb0f73a6fdafbcd84477cc02fa0721e41cc918ee
Mauro Almiron <mdavid.almiron@gmail.com>
2023-10-15 22:53:38 -0300

    changed: we render edit and show in the modal

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -46,6 +46,7 @@ class PlayersController < ApplicationController
   def update
     respond_to do |format|
       if @player.update(player_params)
+        format.turbo_stream { render turbo_stream: turbo_stream.replace( @player, @player ) }
         format.html { redirect_to player_url(@player), notice: "Player was successfully updated." }
         format.json { render :show, status: :ok, location: @player }
       else
